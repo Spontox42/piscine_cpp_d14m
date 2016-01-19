fa3cddb541964582463e24678cf353cafcb8460a
petit_x <petit_x@epitech.eu>
2016-01-19 16:03:49 +0100

    fuck

--- ex00/FruitNode.h
+++ ex00/FruitNode.h
@@ -1,3 +1,13 @@
+/*
+** FruitNode.h for piscine_cpp in /home/petit_x/Epitech/rendu/piscine_cpp/piscine_cpp_d14m/ex00
+** 
+** Made by Marlon Petit
+** Login   <petit_x@epitech.net>
+** 
+** Started on  Tue Jan 19 14:25:17 2016 Marlon Petit
+** Last update Tue Jan 19 14:25:18 2016 Marlon Petit
+*/
+
 #ifndef FRUITNODE_H_
 # define FRUITNODE_H_
 
